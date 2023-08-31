0f30756a21f8e0aa65a4029a6a3402405c21d881
mitchel buyengo <mitchelongachi63@gmail.com>
2023-08-31 20:47:05 +0300

    done

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react'
+function ParentComponent(){
+    const [firstName, setFirstName] = useState("John");
+    const [lastName, setLastName] = useState("Henry");
+  
+    function handleFirstNameChange(event) {
+      setFirstName(event.target.value);
+    }
+  
+    function handleLastNameChange(event) {
+      setLastName(event.target.value);
+    }
+  
+    return (
+      <Form
+        firstName={firstName}
+        lastName={lastName}
+        handleFirstNameChange={handleFirstNameChange}
+        handleLastNameChange={handleLastNameChange}
+      />
+    );
+
+}
+export default ParentComponent;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import Form from './components/Form'
 
 ReactDOM.render(
-  <Form />,
+  <ParentComponent />,
   document.getElementById('root')
 );
